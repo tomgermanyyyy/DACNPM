7af08305a52bd641b80d8cceb6327e6ae528babe
Tran Quoc Viet <tranquocviet26701@gmail.com>
2022-04-06 18:28:07 +0700

    update ui home-page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Sidebar, Home } from '../components';
-
+import { Home, Sidebar } from '../components';
 import '../styles/HomePage.css';
 
 const HomePage = () => {
-    return (
-        <div className="homepage-container">
-            <Sidebar />
-            <Home />
-        </div>
-    );
+  return (
+    <div className="homepage-container">
+      <Sidebar />
+      <Home />
+    </div>
+  );
 };
 
 export default HomePage;
