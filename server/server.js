d04a28229ac74286c2077aa9e249142ff448b11a
HIT2k1 <mainhien2001@gmail.com>
2022-04-17 20:29:32 +0700

    singleton design pattern for database

--- server/server.js
+++ server/server.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const cors = require("cors");
+const GlobalServer = require("./config/db");
 const app = express();
 
-connectDB();
+GlobalServer.getInstance();
 
 app.use(cors());
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    })
-);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (_req, res) => res.send('API Running'));
 app.use('/api/adafruit', require('./routes/adafruit.js'))
 app.use('/api/plot', require('./routes/PlotRouter'));
 
-// app.use('/api/users', require('./routes/users'));
-// app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/UserRouter'));
+app.use('/api/auth', require('./routes/auth'));
 app.use('/api/plot', require('./routes/PlotRouter'));
 app.use('/api/history', require('./routes/HistoryRouter'));
 
